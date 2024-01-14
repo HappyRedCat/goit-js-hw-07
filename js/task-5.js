1cf4acf41551e4039de121b199143ee17cb31dec
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-14 17:49:10 +0100

    up5

---
+++ js/task-5.js
@@ -0,0 +1,13 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16787215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeButton = document.querySelector('.change-color');
+
+changeButton.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+});
