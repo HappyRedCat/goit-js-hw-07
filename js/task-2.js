12572574f1a9754913e98a78d1f534a39a8d25f5
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-14 11:47:53 +0100

    up0

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+class Storage {
+  constructor(arr) {
+    this.items = arr;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === itemToRemove) {
+        this.items.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
